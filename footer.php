f558580fc950c4f1f7928626a23e2899e176dd4c
AnisZemali <125288807+AnisZemali@users.noreply.github.com>
2023-05-28 13:51:32 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,61 @@
+<footer> 
+    <div class="footer">
+      <div class="container footer-container">
+        <div class="footer-left">
+          <a class="footer-logo__link" href="index.php"
+            ><img
+              class="footer-logo"
+              src="img/white.png";
+              alt="White logo"
+              width="150"
+              height="80"
+          /></a>
+          <p class="footer-left__text">
+            DAFTAR-E provides progressive, and affordable healthcare, accessible
+            on mobile and online for everyone
+          </p>
+          <p class="footer-left__copy">
+            ©DAFTAR-E PTY LTD 2023. All rights reserved
+          </p>
+        </div>
+
+        <div class="footer-col">
+          <h3 class="footer-col__heading">Company</h3>
+          <ul class="footer-col__list">
+            <li class="footer-col__list-item">
+              <a class="footer-col__list-link" href="#">Home</a>
+            </li>
+            <li class="footer-col__list-item">
+              <a class="footer-col__list-link" href="#">About Us</a>
+            </li>
+            <li class="footer-col__list-item">
+              <a class="footer-col__list-link" href="#">services</a>
+            </li>
+            <li class="footer-col__list-item">
+              <a class="footer-col__list-link" href="#">Register</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="footer-col">
+          <h3 class="footer-col__heading">Help</h3>
+          <ul class="footer-col__list">
+            <li class="footer-col__list-item">
+              <a class="footer-col__list-link" href="#">Help center</a>
+            </li>
+            <li class="footer-col__list-item">
+              <a class="footer-col__list-link" href="#">Contact support</a>
+            </li>
+            <li class="footer-col__list-item">
+              <a class="footer-col__list-link" href="#">Instructions</a>
+            </li>
+            <li class="footer-col__list-item">
+              <a class="footer-col__list-link" href="#">How it works</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- End of container -->
+    </div>
+    <!-- End of footer -->
+</footer>
---
+++ header.php
@@ -0,0 +1,32 @@
+<head>
+
+    <div class="navbar">
+      <div class="container navbar-container">
+        <a href="index.php">
+          <img
+            class="logo"
+            src="img/logo.png"
+            width="160"
+            height="80"
+            alt="Trafalgar logo"
+          />
+        </a>
+        <ul class="navbar-list">
+          <li class="active navbar-list__item">
+            <a class="navbar-list__item-link" href="index.php">Home</a>
+          </li>
+          <li class="navbar-list__item">
+            <a class="navbar-list__item-link" href="#">About</a>
+          </li>
+          <li class="navbar-list__item">
+            <a class="navbar-list__item-link" href="">services</a>
+          </li>
+          <li class="navbar-list__item">
+            <a class="navbar-list__item-link" href="role.php">Register</a>
+          </li>
+        </ul>
+      </div>
+      <!-- End of container -->
+    </div>
+    <!-- End of navbar -->
+  </head>
